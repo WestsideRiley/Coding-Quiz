419c796c0156b7034274594b7d93a6fe4785aaee
Wesley Riley <Wesley.A.Riley@gmail.com>
2022-05-15 17:34:02 -0500

    Addition of first question and formatting of overall quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,58 @@
 var timerEl = document.getElementById('countdown');
 
+
 function countdown() {
-  var timeLeft = 1;
+  var timeLeft =  120;
   var timeInterval = setInterval(function () {
     if (timeLeft > 1) {
-      timerEl.textContent = timeLeft + ' seconds remaining';
+      timerEl.textContent = "Timer: " + timeLeft;
       timeLeft--;
     } 
     else if (timeLeft === 1) {
-      timerEl.textContent = timeLeft + ' second remaining';
+      timerEl.textContent = "Timer: " + timeLeft;
       timeLeft--;
     } 
     else {
       timerEl.textContent = '';
       clearInterval(timeInterval);
-      displayMessage();
     }
   }, 1000);
 }
 
-countdown();
+
+function StartQuiz(){
+
+document.getElementById("StartQuiz").style.display="none";
+
+document.getElementById('StartQuiz').id='Next1';
+
+document.getElementById("question").innerText="A data type that can be represented as a value of true or false";
+
+document.getElementById("op1").innerText="Boolean";
+document.getElementById("op2").innerText="String";
+document.getElementById("op3").innerText="Element";
+document.getElementById("op4").innerText="Array";
+
+document.getElementById("op1").onclick = function(){
+var createResponse=document.getElementById("options");
+createResponse.innerHTML+= "Correct";
+}
+
+document.getElementById("op2").onclick = function(){
+var createResponse=document.getElementById("options");
+createResponse.innerHTML+= "Incorrect";
+
+}
+
+document.getElementById("op3").onclick = function(){
+var createResponse=document.getElementById("options");
+createResponse.innerHTML+= "Incorrect";
+}
+
+document.getElementById("op4").onclick = function(){
+var createResponse=document.getElementById("options");
+createResponse.innerHTML+= "Incorrect";
+}
+
+
+};
